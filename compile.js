9c36fccd9591c10c437a6d873e35936040fdf24f
Qasim Laghari <go.jets.laghari@gmail.com>
2018-05-20 13:26:45 -0500

    Configure solidity compiler

--- compile.js
+++ compile.js
@@ -1,2 +1,10 @@
 const path = require('path');
 const fs = require('fs');
+const solc = require('solc');
+
+//Set path to fetch solidity file that needs to be compiled
+const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol');
+const source = fs.readFileSync(inboxPath, 'utf8');
+
+//Call the solidity compiler
+module.exports = solc.compile(source, 1);
