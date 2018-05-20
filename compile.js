bd305943a8a74702f0920cfa055dfdd57a3a814a
Qasim Laghari <go.jets.laghari@gmail.com>
2018-05-20 13:34:09 -0500

    Export solidity byte code and abi

--- compile.js
+++ compile.js
@@ -7,4 +7,4 @@ const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol');
 const source = fs.readFileSync(inboxPath, 'utf8');
 
 //Call the solidity compiler
-module.exports = solc.compile(source, 1);
+module.exports = solc.compile(source, 1).contracts[':Inbox'];
