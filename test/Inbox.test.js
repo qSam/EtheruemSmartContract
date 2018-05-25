5c62267d6ee07d0e2f6cc54eb051f1d363566014
Qasim Laghari <go.jets.laghari@gmail.com>
2018-05-25 17:25:52 -0500

    Update test script

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,4 +1,4 @@
-const asset = require('assert');
+const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
