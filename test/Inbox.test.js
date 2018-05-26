4519678b13e98f689a2750209a36fe9413cd4303
Qasim Laghari <go.jets.laghari@gmail.com>
2018-05-26 09:48:52 -0500

    Update Promise with async await

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -3,14 +3,15 @@ const ganache = require('ganache-cli');
 const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
 
-beforeEach(() => {
+let accounts;
+beforeEach(async () => {
   //Get a list of all accounts from Ganache
-  web3.eth.getAccounts().then(fetchedAccounts => {
-    console.log(fetchedAccounts);
-  });
+  accounts = await web3.eth.getAccounts();
   //Use one of those accounts
 });
 
 describe('Inbox Contract', () => {
-  it('deploys a contract', () => {});
+  it('deploys a contract', () => {
+    console.log(accounts);
+  });
 });
