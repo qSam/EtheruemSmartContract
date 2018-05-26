bf314c65ffb9ff00df347a6617eb5fe6298ee702
Qasim Laghari <go.jets.laghari@gmail.com>
2018-05-26 09:45:01 -0500

    Get initial test acounts from Ganache

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -2,3 +2,15 @@ const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
+
+beforeEach(() => {
+  //Get a list of all accounts from Ganache
+  web3.eth.getAccounts().then(fetchedAccounts => {
+    console.log(fetchedAccounts);
+  });
+  //Use one of those accounts
+});
+
+describe('Inbox Contract', () => {
+  it('deploys a contract', () => {});
+});
